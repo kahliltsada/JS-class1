36fd374a5af41d20dda54a24ab0a873100aca3cb
kahliltsada <kahliltsada@gmail.com>
2022-08-24 06:48:55 -0700

    Javacript Assignment 1

---
+++ script.js
@@ -0,0 +1,66 @@
+let userName= "Ksada";
+let password= "0704";
+let firstName= "Kahlil-Taras";
+let lastName= "Sada";
+let phone= "5615066972";
+let email= "kahliltsada@gmail.com";
+let language= "English";
+let paymentMethod= "Visa";
+let planDetails= "Basic plan";
+let creditCard= "1234 5678 9712 3456";
+let expirationDate= "2030-05-09";
+let securityCode= "999";
+let zipCode= "999999";
+let billingDate= "1st of each month";
+let movieGenre= "Action Movie"
+let profileFont= "Times New Roman";
+let viewingRestriction= "Children";
+let profileImage= "none";
+let fontSize= "10px";
+let profileShadow= "none";
+
+console.log(userName);
+console.log(password);
+console.log(firstName);
+console.log(lastName);
+console.log(phone);
+console.log(email);
+console.log(language);
+console.log(paymentMethod);
+console.log(planDetails);
+console.log(creditCard);
+console.log(expirationDate);
+console.log(securityCode);
+console.log(zipCode);
+console.log(billingDate);
+console.log(movieGenre);
+console.log(fontSize);
+console.log(viewingRestriction);
+console.log(profileImage);
+console.log(fontSize);
+console.log(profileShadow);
+
+document.write(`
+<p>Username: ${userName}</p>
+<p>Password: ${password}</p>
+<p>First Name: ${firstName}</p>
+<p>Last Name: ${lastName}</p>
+<p>Phone: ${phone}</p>
+<p>Email: ${email}</p>
+<p>Language: ${language}</p>
+<p>Payment Method: ${paymentMethod}</p>
+<p>Credit Card: ${creditCard}</p>
+<p>ExpirationDate: ${expirationDate}</p>
+<p>Security Code: ${securityCode}</p>
+<p>Zip Code: ${zipCode}</p>
+<p>Plan Details: ${planDetails}</p>
+<p>Billing Date: ${billingDate}</p>
+<p>Viewing Restrictions: ${viewingRestriction}</p>
+<p>Profile Image: ${profileImage}</p>
+<p>Profile Font Size: ${fontSize}</p>
+<p>Profile Shadows: ${profileShadow}
+<p>`)
+
+
+
+
